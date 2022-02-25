85790d0631e25db186378d6b5f60fec379027a92
Mukesh Kumar <65598381+mukesh154@users.noreply.github.com>
2022-02-25 13:05:43 +0530

    Update Calculator.java

--- src/main/java/com/cognitree/levelops/codebuild/Calculator.java
+++ src/main/java/com/cognitree/levelops/codebuild/Calculator.java
@@ -2,18 +2,22 @@ package com.cognitree.levelops.codebuild;
 
 public class Calculator {
 
+    //addition
     public int add(int x, int y) {
         return x + y;
     }
 
+    //subtraction
     public int subtract(int x, int y) {
         return x - y;
     }
 
+    //multiplication
     public int multiply(int x, int y) {
         return x * y;
-    }
+    }    //addition
 
+    //division
     public int divide(int x, int y) {
         return x / y;
     }
